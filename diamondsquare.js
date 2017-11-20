a8facac91376dac59ce984bec022ec7d7dc007a7
Ben Wiley <therealbenwiley@gmail.com>
2017-11-20 01:19:43 -0500

    Basic diamond square implemented (without random displacement).

--- diamondsquare.js
+++ diamondsquare.js
@@ -13,22 +13,32 @@ topRightHeightInput.addEventListener('input', render);
 bottomLeftHeightInput.addEventListener('input', render);
 bottomRightHeightInput.addEventListener('input', render);
 
+render();
+
 function render () {
-  const level = levelInput.value;
+  const level = Number(levelInput.value);
   const span = 2 ** level + 1;
   textureCanvas.width = span;
   textureCanvas.height = span;
 
-  // compute heights via Diamond Square algorithm
-  let maxHeight = 0;
+  // initialize height data container
   const heightData = Array(span);
   for (let x = 0; x < span; x++) {
     heightData[x] = Array(span);
-    for (let y = 0; y < span; y++) {
-      heightData[x][y] = 5 + Math.random() * 10;
-      maxHeight = Math.max(maxHeight, heightData[x][y]);
-    }
   }
+
+  // compute heights via Diamond Square algorithm
+  heightData[0][0] = Number(topLeftHeightInput.value);
+  heightData[span - 1][0] = Number(topRightHeightInput.value);
+  heightData[0][span - 1] = Number(bottomLeftHeightInput.value);
+  heightData[span - 1][span - 1] = Number(bottomRightHeightInput.value);
+  const maxHeight = Math.max(
+    heightData[0][0],
+    heightData[span - 1][0],
+    heightData[0][span - 1],
+    heightData[span - 1][span - 1],
+    diamondSquare(heightData, level) // performs diamond square algorithm
+  );
 
   // convert heights to image data for canvas
   const imageData = ctx.createImageData(span, span);
@@ -46,4 +56,78 @@ function render () {
   ctx.putImageData(imageData, 0, 0);
 }
 
-render();
+// implementation based on visual description at:
+// https://en.wikipedia.org/wiki/Diamond-square_algorithm#Visualization
+// NOTE: mutates heightData.
+function diamondSquare (heightData, level) {
+  let maxHeight = 0;
+
+  if (level < 1) {
+    return maxHeight;
+  }
+
+  const interval = 2 ** level;
+  const half = 2 ** (level - 1);
+
+  // diamond step
+  for (let yOffset = 0; yOffset + 1 < heightData.length; yOffset += interval) {
+    for (let xOffset = 0; xOffset + 1 < heightData.length; xOffset += interval) {
+      heightData[xOffset + half][yOffset + half] = (
+        heightData[xOffset + 0][yOffset + 0] +
+        heightData[xOffset + half * 2][yOffset + 0] +
+        heightData[xOffset + 0][yOffset + half * 2] +
+        heightData[xOffset + half * 2][yOffset + half * 2]
+      ) / 4 + 0 /* TODO: replace 0 with random number */;
+
+      maxHeight = Math.max(
+        maxHeight,
+        heightData[xOffset + half][yOffset + half]
+      );
+    }
+  }
+
+  // square step
+  for (let yOffset = 0; yOffset + 1 < heightData.length; yOffset += interval) {
+    for (let xOffset = 0; xOffset + 1 < heightData.length; xOffset += interval) {
+      const noTopCorner = yOffset <= 0;
+      heightData[xOffset + half][yOffset + 0] = (
+        (noTopCorner ? 0 : heightData[xOffset + half][yOffset - half]) +
+        heightData[xOffset + half][yOffset + half] +
+        heightData[xOffset + 0][yOffset + 0] +
+        heightData[xOffset + half * 2][yOffset + 0]
+      ) / (noTopCorner ? 3 : 4) + 0 /* TODO: replace 0 with random number */;
+      const noBottomCorner = yOffset + half * 2 + 1 >= heightData.length;
+      heightData[xOffset + half][yOffset + half * 2] = (
+        heightData[xOffset + half][yOffset + half] +
+        (noBottomCorner ? 0 : heightData[xOffset + half][yOffset + half * 3]) +
+        heightData[xOffset + 0][yOffset + half * 2] +
+        heightData[xOffset + half * 2][yOffset + half * 2]
+      ) / (noBottomCorner ? 3 : 4) + 0 /* TODO: replace 0 with random number */;
+      const noLeftCorner = xOffset <= 0;
+      heightData[xOffset + 0][yOffset + half] = (
+        heightData[xOffset + 0][yOffset + 0] +
+        heightData[xOffset + 0][yOffset + half * 2] +
+        (noLeftCorner ? 0 : heightData[xOffset - half][yOffset + half]) +
+        heightData[xOffset + half][yOffset + half]
+      ) / (noLeftCorner ? 3 : 4) + 0 /* TODO: replace 0 with random number */;
+      const noRightCorner = xOffset + half * 2 + 1 >= heightData.length;
+      heightData[xOffset + half * 2][yOffset + half] = (
+        heightData[xOffset + half * 2][yOffset + 0] +
+        heightData[xOffset + half * 2][yOffset + half * 2] +
+        heightData[xOffset + half][yOffset + half] +
+        (noRightCorner ? 0 : heightData[xOffset + half * 3][yOffset + half])
+      ) / (noRightCorner ? 3 : 4) + 0 /* TODO: replace 0 with random number */;
+
+      maxHeight = Math.max(
+        maxHeight,
+        heightData[xOffset + half][yOffset + half],
+        heightData[xOffset + half][yOffset + 0],
+        heightData[xOffset + half][yOffset + half * 2],
+        heightData[xOffset + 0][yOffset + half],
+        heightData[xOffset + half * 2][yOffset + half]
+      );
+    }
+  }
+
+  return Math.max(maxHeight, diamondSquare(heightData, level - 1));
+}
